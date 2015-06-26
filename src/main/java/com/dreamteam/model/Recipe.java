919d89e52e14b800fe3687f198ad695b9d9a9bd6
chemiczny <glanowskimichal@gmail.com>
2015-06-26 19:18:01 +0200

    Deploy

--- src/main/java/com/dreamteam/model/Recipe.java
+++ src/main/java/com/dreamteam/model/Recipe.java
@@ -8,7 +8,7 @@ public class Recipe {
     private String opis;
     private int ocena;
     private int typ;
-    private FileInputStream zdjecie;
+    private File zdjecie;
 
     public long getId_przepis() {
         return id_przepis;
@@ -34,12 +34,22 @@ public class Recipe {
     public void setOcena(int ocena) {
         this.ocena = ocena;
     }
-    public FileInputStream getZdjecie() {
+    public File getZdjecie() {
         return this.zdjecie;
     }
     public void setZdjecie(File zdjecie) {
-        FileInputStream photoBuff = new FileInputStream(zdjecie);
         this.zdjecie = zdjecie;
+    }
+    public int PhotoLen(){
+        return (int)this.zdjecie.length();
+    }
+    public FileInputStream getPhotoStream(){
+        FileInputStream photo=null;
+        try {
+            photo = new FileInputStream(this.zdjecie);
+        }
+        catch (java.io.FileNotFoundException ignored){}
+        return photo;
     }
     public int getTyp() {
         return typ;
