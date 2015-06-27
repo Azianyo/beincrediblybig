e1eacbe2bedd3d40e6e777bccf7f49d3a41c4763
Imielin <maciekbojdak@gmail.com>
2015-06-27 12:47:18 +0200

    Deploy

--- src/main/java/com/dreamteam/dao/recipeDao.java
+++ src/main/java/com/dreamteam/dao/recipeDao.java
@@ -52,7 +52,7 @@ public class recipeDao {
     public void updateRecipe(Recipe przepis) {
         try {
             PreparedStatement preparedStatement = connection.prepareStatement("update przepis set nazwa=? , " +
-                    "opis=? ,ocena=? , typ=?, zdjecie=?" + "where przepis_id=?");
+                    "opis=? ,ocena=? , typ=?, zdjecie=? where przepis_id=?");
             // Parameters start with 1
             preparedStatement.setString(1, przepis.getNazwa());
             preparedStatement.setString(2, przepis.getOpis());
