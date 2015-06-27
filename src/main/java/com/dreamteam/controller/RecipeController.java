0a44640b96a1a591a204217af3d778592c025969
dzejbi <janek.bialkowski@gmail.com>
2015-06-27 11:26:59 +0200

    Deploy

--- src/main/java/com/dreamteam/controller/RecipeController.java
+++ src/main/java/com/dreamteam/controller/RecipeController.java
@@ -15,7 +15,7 @@ import com.dreamteam.model.Recipe;
 public class RecipeController extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static String INSERT_OR_EDIT = "/Recipe.jsp";
-    private static String LIST_RECIPE = "/listRecipe.jsp";
+    private static String LIST_RECIPE = "/index.jsp";
     private recipeDao dao;
 
     public RecipeController() {
@@ -67,8 +67,8 @@ public class RecipeController extends HttpServlet {
             recipe.setId_przepis(Integer.parseInt(recipeId));
             dao.updateRecipe(recipe);
         }
-        //RequestDispatcher view = request.getRequestDispatcher(LIST_RECIPE);
-        //request.setAttribute("przepis", dao.getAllRecipes());
-        //view.forward(request, response);
+        RequestDispatcher view = request.getRequestDispatcher(LIST_RECIPE);
+        request.setAttribute("przepis", dao.getAllRecipes());
+        view.forward(request, response);
     }
 }
