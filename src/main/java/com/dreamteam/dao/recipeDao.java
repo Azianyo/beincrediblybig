ef1638cea9abfb87e71a1f0f8a8ea62bc8df6dc3
Mateusz Gawlik <matoegawel@gmail.com>
2015-06-29 20:43:02 +0200

    Deploy

--- src/main/java/com/dreamteam/dao/recipeDao.java
+++ src/main/java/com/dreamteam/dao/recipeDao.java
@@ -155,9 +155,9 @@ public class recipeDao {
 
         List<Recipe> recipes = new ArrayList<Recipe>();
         try {
-            String selectSQL = "select * from przepis where typ=?";
+            String selectSQL = "select * from przepis where typ=:typ";
             PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
-            preparedStatement.setInt(1, typ);
+            //preparedStatement.setInt(1, typ);
             ResultSet rs = preparedStatement.executeQuery("selectSQL");
 
             while (rs.next()) {
