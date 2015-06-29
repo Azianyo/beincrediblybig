de310a006945a21e0ac9afb870b97d45ed3ea734
Zaeus <wpb1992@gmail.com>
2015-06-29 14:48:21 +0200

    Gmeranie w PDF ciąg dalszy :/

--- src/main/java/com/dreamteam/controller/RecipeController.java
+++ src/main/java/com/dreamteam/controller/RecipeController.java
@@ -20,6 +20,7 @@ public class RecipeController extends HttpServlet {
     private static String INSERT_OR_EDIT = "/Recipe.jsp";
     private static String LIST_RECIPE = "/listRecipes.jsp";
     private static String DIET_GENERATOR = "/dietGenerator.jsp";
+    private static String MAIN = "/index.jps";
     private recipeDao dao;
 
     public RecipeController() {
@@ -49,7 +50,7 @@ public class RecipeController extends HttpServlet {
         } else if (action.equalsIgnoreCase("CreateDiet")){
             forward = DIET_GENERATOR;
         } else if (action.equalsIgnoreCase("generate")){
-            forward = "";
+            forward = MAIN;
             long recipeID = Long.parseLong(request.getParameter("id_przepis"));
             Recipe recipe = dao.getRecipeById(recipeID);
             new PDFGenerator(recipe);
