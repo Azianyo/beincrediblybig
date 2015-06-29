0ece164a391d1c963c04a963dbf89f45a598c965
dzejbi <janek.bialkowski@gmail.com>
2015-06-29 18:06:47 +0200

    Deploy

--- src/main/java/com/dreamteam/model/IngredientWithAmount.java
+++ src/main/java/com/dreamteam/model/IngredientWithAmount.java
@@ -4,6 +4,22 @@ package com.dreamteam.model;
  * Created by Michał on 2015-06-29.
  */
 public class IngredientWithAmount {
-    public int amount;
-    public Ingredient ingredient;
+    private int amount;
+    private Ingredient ingredient;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
 }
