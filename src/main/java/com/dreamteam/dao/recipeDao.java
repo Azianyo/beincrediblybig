b3446ab6435ed47dd9d9962074758a052d2e5d38
Mateusz Gawlik <matoegawel@gmail.com>
2015-06-29 21:24:31 +0200

    Deploy

--- src/main/java/com/dreamteam/dao/recipeDao.java
+++ src/main/java/com/dreamteam/dao/recipeDao.java
@@ -160,7 +160,7 @@ public class recipeDao {
             preparedStatement.setInt(1, typ);
             ResultSet rs = preparedStatement.executeQuery();
 
-            if(rs.next()) {
+            while(rs.next()) {
                 Recipe przepis = new Recipe();
                 przepis.setId_przepis(rs.getLong("id_przepis"));
                 przepis.setNazwa(rs.getString("nazwa"));
